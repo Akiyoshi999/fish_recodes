572a10138e2a4b1d85bc5dce4dc8da713a5b68f2
Akiyoshi999 <zundokokiyoshi999@gmail.com>
2021-01-09 16:43:21 +0900

    layoutにrecodesテーブルの一覧を表示

--- app/Http/Controllers/RecodeController.php
+++ app/Http/Controllers/RecodeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Recode;
 
 class RecodeController extends Controller
 {
@@ -13,6 +14,10 @@ class RecodeController extends Controller
      */
     public function showList()
     {
-        return view('recode.recodes'); // resourcesのview配下
+        // Recodeの全データを取得する
+        $recodes = Recode::all();
+
+        // resourcesのview配下,第2引数で配列をviewに渡す
+        return view('recode.recodes', ['recodes' => $recodes]);
     }
 }
--- resources/views/recode/recodes.blade.php
+++ resources/views/recode/recodes.blade.php
@@ -5,73 +5,39 @@
     <div class="container">
 
         <div class="row">
-            <div class="col-md-4">
-                <div class="card mb-4 shadow-sm">
-                    <img class="card-img-top"
-                        data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail"
-                        alt="Card image cap">
-                    <div class="card-body">
-                        <!-- <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p> -->
-                        <p class="card-text">これは写真の解説文付きのカードです。自然に説明を加えることができます。しかしこの文章は少し長いかもしれません。</p>
-                        <div class="d-flex justify-content-between align-items-center">
-                            <div class="btn-group">
-                                <!-- <button type="button" class="btn btn-sm btn-outline-secondary">View</button> -->
-                                <button type="button" class="btn btn-sm btn-outline-secondary">見る</button>
-                                <!-- <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button> -->
-                                <button type="button" class="btn btn-sm btn-outline-secondary">編集</button>
-                            </div>
-                            <!-- <small class="text-muted">9 mins</small> -->
-                            <small class="text-muted">9分</small>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card mb-4 shadow-sm">
-                    <img class="card-img-top"
-                        data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail"
-                        alt="Card image cap">
-                    <div class="card-body">
-                        <!-- <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p> -->
-                        <p class="card-text">これは写真の解説文付きのカードです。自然に説明を加えることができます。しかしこの文章は少し長いかもしれません。</p>
-                        <div class="d-flex justify-content-between align-items-center">
-                            <div class="btn-group">
-                                <!-- <button type="button" class="btn btn-sm btn-outline-secondary">View</button> -->
-                                <button type="button" class="btn btn-sm btn-outline-secondary">見る</button>
-                                <!-- <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button> -->
-                                <button type="button" class="btn btn-sm btn-outline-secondary">編集</button>
-                            </div>
-                            <!-- <small class="text-muted">9 mins</small> -->
-                            <small class="text-muted">9分</small>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card mb-4 shadow-sm">
-                    <img class="card-img-top"
-                        data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail"
-                        alt="Card image cap">
-                    <div class="card-body">
-                        <!-- <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p> -->
-                        <p class="card-text">これは写真の解説文付きのカードです。自然に説明を加えることができます。しかしこの文章は少し長いかもしれません。</p>
-                        <div class="d-flex justify-content-between align-items-center">
-                            <div class="btn-group">
-                                <!-- <button type="button" class="btn btn-sm btn-outline-secondary">View</button> -->
-                                <button type="button" class="btn btn-sm btn-outline-secondary">見る</button>
-                                <!-- <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button> -->
-                                <button type="button" class="btn btn-sm btn-outline-secondary">編集</button>
-                            </div>
-                            <!-- <small class="text-muted">9 mins</small> -->
-                            <small class="text-muted">9分</small>
-                        </div>
-                    </div>
-                </div>
-            </div>
 
-
+            @foreach($recodes as $recode)
+            <div class="col-md-4">
+                <div class="card mb-4 shadow-sm">
+                    <img class="card-img-top"
+                        data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail"
+                        alt="Card image cap">
+                    <div class="card-body">
+                        <div class="simple-contents">
+                            <p>日時：{{ $recode->date }}</p>
+                            <p>場所：{{ $recode->place}}</p>
+                            <p>長さ：{{ $recode->length }}cm</p>
+                        </div>
+                        <!-- <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p> -->
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <!-- <button type="button" class="btn btn-sm btn-outline-secondary">View</button> -->
+                                <button type="button" class="btn btn-sm btn-outline-secondary">詳細</button>
+                                <!-- <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button> -->
+                                <button type="button" class="btn btn-sm btn-outline-secondary">編集</button>
+                                <button type="button" class="btn btn-sm btn-outline-secondary">削除</button>
+                            </div>
+                            <!-- <small class="text-muted">9 mins</small> -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+            @endforeach
         </div>
+
+
     </div>
+</div>
 </div>
 </div>
 @endsection
